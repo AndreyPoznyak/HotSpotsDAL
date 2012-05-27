e1a6f22740af5040d4af9985dc412a392d31ddd0
AndreyPoznyak <andpoznyak@gmail.com>
2012-05-27 22:11:41 +0300

    first commit

---
+++ HotSpotsDAL/HotSpotDAL.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using System.Data;
+
+namespace HotSpotsConnectedLayer
+{
+    public static class HotSpotDAL
+    {
+        public static void AddHotSpot(HotSpot obj)
+        {
+            using (var context = new HotSpotsDBEntities())
+            {
+                context.AddToHotSpot(obj);
+                context.SaveChanges();
+            }
+        }
+
+        public static void DeleteHotSpot(String BssidToDelete)
+        {
+            HotSpot obj = new HotSpot();
+            using (var context = new HotSpotsDBEntities())
+            {
+                EntityKey key = new EntityKey("HotSpotsDBEntities.Tour", "bssid", BssidToDelete);
+                obj = (HotSpot)context.GetObjectByKey(key);
+                if (obj != null)
+                {
+                    context.DeleteObject(obj);
+                }
+                context.SaveChanges();
+            }
+        }
+
+        public static List<HotSpot> GetAllHotSpots()
+        {
+            List<HotSpot> list = new List<HotSpot>();
+            using (var context = new HotSpotsDBEntities())
+            {
+                var ListOfHotSpots = from item in context.HotSpot select item;
+                list = (List<HotSpot>)ListOfHotSpots;
+            }
+            return list;
+        }
+
+        public static HotSpot GetHotSpot(String BssidToGet)
+        {
+            HotSpot obj = new HotSpot();
+            using (var context = new HotSpotsDBEntities())
+            {
+                EntityKey key = new EntityKey("HotSpotsDBEntities.HotSpot", "bssid", BssidToGet);
+                obj = (HotSpot)context.GetObjectByKey(key);
+            }
+            return obj;
+        }
+    }
+}
